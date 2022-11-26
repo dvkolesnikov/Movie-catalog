369c82b38f0e1aba921ff694f8c527e0e4648147
Dmitry Kolesnikov <kolesnikov.nsk@gmail.com>
2022-11-26 12:37:47 +0600

    Upgrade gradle and dependencies versions

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -3,27 +3,26 @@ import org.gradle.api.artifacts.dsl.DependencyHandler
 //version constants for the Kotlin DSL dependencies
 object Versions {
     //app level
-    const val gradle = "7.2.2"
-    const val kotlin = "1.7.10"
-    const val kotlinComposeCompiler = "1.3.0"
+    const val gradle = "7.3.1"
+    const val kotlin = "1.7.20"
+    const val kotlinComposeCompiler = "1.3.2"
 
     //libs
     const val coil = "2.2.2"
-    const val coreKtx = "1.8.0"
-    const val appcompat = "1.4.2"
-    const val compose = "1.3.0"
+    const val coreKtx = "1.9.0"
+    const val compose = "1.3.1"
     const val composeMaterial3 = "1.0.1"
-    const val composeActivity = "1.5.1"
-    const val composeNavigation = "2.5.1"
-    const val koin = "3.2.0"
-    const val moshi = "1.13.0"
+    const val composeActivity = "1.6.1"
+    const val composeNavigation = "2.5.3"
+    const val koin = "3.3.0"
+    const val moshi = "1.14.0"
     const val okhttp = "4.10.0"
     const val retrofit = "2.9.0"
 
     //test
-    const val junit = "4.12"
-    const val extJunit = "1.1.1"
-    const val espresso = "3.2.0"
+    const val junit = "4.13.2"
+    const val extJunit = "1.1.4"
+    const val espresso = "3.4.0"
 }
 
 object AppDependencies {
@@ -31,10 +30,9 @@ object AppDependencies {
     private const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
 
     //android ui
-    private const val appcompat = "androidx.appcompat:appcompat:${Versions.appcompat}"
     private val coil = "io.coil-kt:coil-compose:${Versions.coil}"
     private val compose = listOf(
-        "androidx.compose.ui:ui:${Versions.compose}",
+        "androidx.compose.ui:ui:1.3.0",
         "androidx.compose.ui:ui-tooling:${Versions.compose}",
         "androidx.compose.foundation:foundation:${Versions.compose}",
         "androidx.compose.material:material:${Versions.compose}",
@@ -52,7 +50,7 @@ object AppDependencies {
         "io.insert-koin:koin-androidx-compose:${Versions.koin}"
     )
 
-    val network = listOf(
+    private val network = listOf(
         "com.squareup.okhttp3:okhttp:${Versions.okhttp}",
         "com.squareup.okhttp3:logging-interceptor:${Versions.okhttp}",
         "com.squareup.retrofit2:retrofit:${Versions.retrofit}",
@@ -69,7 +67,6 @@ object AppDependencies {
     val appLibraries = arrayListOf<String>().apply {
         add(kotlinStdLib)
         add(coreKtx)
-        add(appcompat)
         addAll(compose)
         addAll(koinAndroid)
         addAll(network)
